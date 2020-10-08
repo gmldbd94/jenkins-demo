236b225be8332e4a39145a02899cb2c63184a45b
gmldbd94 <gmldbd94@gmail.com>
2020-10-08 11:23:43 +0900

    add git repository

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('openshy4j-project'){
+    definition{
+        cpsScm{
+            scm{
+                git{
+                    remote{
+                        url 'https://github.com/gmldbd94/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
